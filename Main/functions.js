f78784c43fe4280b59900de4d4f864ad16e08ee4
Suresh Amgoth <amgothsuresh333@gmail.com>
2025-08-08 12:43:35 +0200

    updated bootstrap color function

--- Main/functions.js
+++ Main/functions.js
@@ -23,7 +23,7 @@ export function getMaxDepth(data) {
 export function getLinkColorByBootStrap(BootstrapValue) {
   const num = parseFloat(BootstrapValue);
   if (num > 0.9) return 'green';        // above 0.9 → green
-  else if (num >= 0.6 && num <= 0.9) return 'orange'; // between 0.6 and 0.9 → orange
+  else if (num >= 0.6 && num <= 0.999) return 'orange'; // between 0.6 and 0.9 → orange
   return 'red';                         // below 0.6 → red
 }
 
